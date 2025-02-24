2992266fc1b2c75454b01a034d71e17b8128ce26
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-02-24 14:36:22 +0300

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render, Query } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getIndexPage(@Query('user') user: string) {
+    return { session: user ? { name: user, authorized: true } : { authorized: false }, title: 'Главная' };
+  }
+
+  @Get('about')
+  @Render('about')
+  getAboutPage() {
+    return { title: 'О нас' };
+  }
+
+  @Get('services')
+  @Render('services')
+  getServicesPage() {
+    return { title: 'Услуги' };
+  }
+
+  @Get('contact')
+  @Render('contact')
+  getContactPage() {
+    return { title: 'Контакты' };
+  }
+
+  @Get('reviews')
+  @Render('reviews')
+  getReviewsPage() {
+    return { title: 'Отзывы' };
+  }
+
+  @Get('service-info')
+  @Render('service-info')
+  getServiceInfoPage() {
+    return { title: 'Добавить услугу' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Указываем шаблонизатор hbs
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  // Отдача статики (CSS, JS, картинки)
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Приложение запущено на http://localhost:${port}`);
 }
 bootstrap();
