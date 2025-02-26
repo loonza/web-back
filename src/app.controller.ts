55ddd61dbb0b2c96d813163ae77b5d75cde1787b
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-02-27 02:46:16 +0300

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,25 +18,25 @@ export class AppController {
     }
   }
 
-  @Get('/views/index')
-  @Render('/views/index') // Главная после входа
+  @Get('/index')
+  @Render('/index') // Главная после входа
   getHome(@Query('user') user: string) {
     return { title: 'Главная страница', user: user ? { name: user } : null };
   }
 
-  @Get('/views/about')
-  @Render('/views/about')
+  @Get('/about')
+  @Render('/about')
   getAnimals(@Query('user') user: string) {
     return { title: 'О нас', user: user ? { name: user } : null };
   }
 
-  @Get('/views/services')
+  @Get('/services')
   @Render('services')
   getServices(@Query('user') user: string) {
     return { title: 'Услуги', user: user ? { name: user } : null };
   }
 
-  @Get('/views/contacts')
+  @Get('/contacts')
   @Render('contacts')
   getContacts(@Query('user') user: string) {
     return { title: 'Контакты', user: user ? { name: user } : null };
