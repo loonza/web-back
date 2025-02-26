8d863ced578ce07c5387074f17633bf979f869e5
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-02-27 00:17:08 +0300

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,54 @@
-import { Controller, Get, Render, Query } from '@nestjs/common';
+import { Controller, Get, Render, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  @Get()
-  @Render('index')
-  getIndexPage(@Query('user') user: string) {
-    return { session: user ? { name: user, authorized: true } : { authorized: false }, title: 'Главная' };
+  @Get('/')
+  @Render('auth')
+  getLoginPage() {
+    return { title: 'Авторизация' };
   }
 
-  @Get('about')
+  @Get('/login')
+  login(@Query('user') user: string, @Res() res: Response) {
+    if (user) {
+      res.redirect(`/home?user=${encodeURIComponent(user)}`);
+    } else {
+      res.redirect('/');
+    }
+  }
+
+  @Get('/home')
+  @Render('index')
+  getHome(@Query('user') user: string) {
+    return { title: 'Главная страница', session: user ? { name: user, authorized: true } : { authorized: false } };
+  }
+
+  @Get('/about')
   @Render('about')
   getAboutPage() {
     return { title: 'О нас' };
   }
 
-  @Get('services')
+  @Get('/services')
   @Render('services')
   getServicesPage() {
     return { title: 'Услуги' };
   }
 
-  @Get('contact')
+  @Get('/contact')
   @Render('contact')
   getContactPage() {
     return { title: 'Контакты' };
   }
 
-  @Get('reviews')
+  @Get('/reviews')
   @Render('reviews')
   getReviewsPage() {
     return { title: 'Отзывы' };
   }
 
-  @Get('service-info')
+  @Get('/service-info')
   @Render('service-info')
   getServiceInfoPage() {
     return { title: 'Добавить услугу' };
