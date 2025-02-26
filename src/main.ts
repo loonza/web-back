0b95d667ff975ef6db823569a383e23517378556
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-02-26 22:49:28 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  try {
 
-  app.useStaticAssets(join(__dirname, 'public'), {
-    prefix: '/',
-  });
+    app.useStaticAssets(join(__dirname, 'public'), {
+      prefix: '/',
+    });
 
-  app.setBaseViewsDir(join(__dirname, 'views'));
-  app.setViewEngine('hbs');
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 Сервер запущен на http://localhost:${port}`);
+    app.setBaseViewsDir(join(__dirname, 'views'));
+    app.setViewEngine('hbs');
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`🚀 Сервер запущен на http://localhost:${port}`);
+  } catch (error) {
+    console.error('🔥 Ошибка при запуске сервера:', error);
+  }
 }
 bootstrap();
