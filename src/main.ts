0ac0f755b1814c1c761427da6127f3d28ed5571f
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-02-26 22:45:43 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Указываем шаблонизатор hbs
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
 
-  // Отдача статики (CSS, JS, картинки)
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, 'public'), {
+    prefix: '/',
+  });
+
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Приложение запущено на http://localhost:${port}`);
+  console.log(`🚀 Сервер запущен на http://localhost:${port}`);
 }
 bootstrap();
