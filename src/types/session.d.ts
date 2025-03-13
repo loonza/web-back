67bba1cd6910aba41af700e989b3af8e94188453
Vafaullin Albert <vafaullin.albert@mail.ru>
2025-03-13 11:40:47 +0300

    commit

--- src/types/session.d.ts
+++ src/types/session.d.ts
@@ -1,7 +1,13 @@
-import 'express-session';
+import { Session } from 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
     user?: string;
+  }
+}
+
+declare module 'express' {
+  interface Request {
+    session: Session & { user?: string };
   }
 }
